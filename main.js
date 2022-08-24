21adc20c333227d29e8150d6f554f21c4908bdbf
hector918 <hector_zhong@outlook.com>
2022-08-23 21:18:34 -0400

    submit events lab

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let current = document.querySelector("#current-color");
+let palette = document.querySelector("#palette");
+let canvas = document.querySelector("#canvas");
+for(let x of palette.childNodes)
+{
+  if(typeof x==="object")
+  {
+    x.addEventListener("click",(evt)=>{
+      current.setAttribute("style",evt.target.getAttribute("style"));
+    })
+  }
+}
+//////////////////////////////////
+for(let x of canvas.childNodes){
+  if(typeof x === "object")
+    x.addEventListener("click",(evt)=>{
+      evt.target.setAttribute("style",current.getAttribute("style"));
+    })
+}
+
+//////extra
+canvas.addEventListener('mousemove', e => {
+  if(e.buttons===1)
+  {
+    let mouseOnEl = document.elementFromPoint(e.clientX, e.clientY);
+    if(mouseOnEl.parentNode==canvas) mouseOnEl.setAttribute("style",current.getAttribute("style"));
+  }
+}, {passive: true})
